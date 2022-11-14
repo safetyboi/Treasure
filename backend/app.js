681eb175ef3405040adf7cef078c99fb4f68189f
Daniel Culbertson <daniel.j.culbertson@gmail.com>
2022-11-14 10:07:17 -0800

    add mern backend

---
+++ backend/app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+// const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+
+const cors = require('cors');
+const { isProduction } = require('./config/keys');
+const csurf = require('csurf');
+const debug = require('debug');
+
+require('./models/User');
+
+require('./config/passport'); // <-- ADD THIS LINE
+const passport = require('passport');
+
+// const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/api/users');
+
+const csrfRouter = require('./routes/api/csrf');
+
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express 
+    // server will serve the React files statically.)
+    app.use(cors());
+  }
+
+  // ...
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
+
+// app.use('/', indexRouter);
+app.use('/api/users', usersRouter);
+
+app.use('/api/csrf', csrfRouter);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+  });
+
+const serverErrorLogger = debug('backend:error');
+
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+  });
+
+module.exports = app;
