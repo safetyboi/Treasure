040268c4187c71316bf5c502bac4bbaea12a660f
fee3fitri <safitri.shelton@gmail.com>
2022-11-15 20:08:38 -0800

    Create event profile

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -8,25 +8,28 @@ const Profile = () => {
   const user = useSelector(state => state.session.user);
 
   return (
-    <section className="profile_wrapper flex-col align-center">
-      <h1>Profile</h1>
-      <div className="profile_content flex-row justify-between align-center">
-        <div className="profile_details flex-row">
-          <picture>
-            <img src='' alt='user_images' />
-          </picture>
-          <div className="user_details">
-            <h2>{user.username}</h2>
-            <h3>{user.email}</h3>
-            <p className="join">Joined in {user.createdAt}</p>
-            <p className='delete'>Delete profile</p>
+    <section className="profile_wrapper flex-col">
+      <h1 className='text-center'>Profile</h1>
+      <div className="profile_content flex-row justify-between">
+        <div className='flex-row align-center'>
+          <div className="profile_details flex-row justify-center">
+            <picture>
+              <img src='' alt='user_images' />
+            </picture>
+            <div className="user_details">
+              <h2>{user.username}</h2>
+              <h3>{user.email}</h3>
+              <p className="join">Joined in {user.createdAt}</p>
+              <p className='delete'>Delete profile</p>
+            </div>
+          </div>
+          <div className="profile_link flex-col align-end">
+            <Link to={'/events/new'}><Button>Create event</Button></Link>
+            <Link to={'/events'}><Button>Find events to join</Button></Link>
           </div>
         </div>
-        <div className="profile_link flex-col align-end">
-          <Link to={'/events/new'}><Button>Create Event</Button></Link>
-          <Link to={'/events'}><Button>Find events to join</Button></Link>
-        </div>
       </div>
+      <div className='border'></div>
       <ProfileEvent />
     </section>
 
--- frontend/src/components/Profile/ProfileEvent.js
+++ frontend/src/components/Profile/ProfileEvent.js
@@ -2,14 +2,16 @@ import './Profile.scss'
 
 const ProfileEvent = () => {
   return (
-    <div className='profile_event_wrapper flex-row'>
-      <div className='joined_event_wrapper flex-col'>
+    <div className='profile_event_wrapper'>
+      <div className='event_wrapper'>
         <h2>Joined Events</h2>
       </div>
       <div className='border'></div>
-      <div className='created_event_wrapper flex-col'>
+      <div className='event_wrapper'>
         <h2>Created events</h2>
+        <p>No events created...yet!</p>
       </div>
+      
     </div>
   );
 }
