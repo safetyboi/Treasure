88f9b8313fb8ce753427c187eb7d97753153503c
fee3fitri <safitri.shelton@gmail.com>
2022-11-16 20:08:04 -0800

    Fixing the date

--- frontend/src/components/Events/EventIndexItem.js
+++ frontend/src/components/Events/EventIndexItem.js
@@ -2,19 +2,22 @@ import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 
 function EventIndexItem({event}) {
-  if (event) {
-    console.log(event._id)
-  }
-
+  const dateVal = event.date;
+  const dateObj = new Date(dateVal);
+  const days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+  const day = dateObj.getDay();
+  const index = dateVal.indexOf('T');
+  const date = dateVal.slice(0, index);
+  
   return (
     <li className="event_index_list">
       <div className="flex-row">
         <div className="event_list_details">
           <h2>{event.name}</h2>
           <p>{event.location}</p>
-          <p>{event.date}</p>
-          <p>{event.duration}</p>
-          <p>{event.price}</p>
+          <p>{days[day]}, {date}</p>
+          <p>{event.duration} minutes</p>
+          <p>${event.price}</p>
         </div>
         <div className="event_list_img">
           <picture>
