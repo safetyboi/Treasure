8d651e4ad4a229fcebec2c99b0d2c04ed57c0d26
fee3fitri <safitri.shelton@gmail.com>
2022-11-16 20:43:51 -0800

    Fixing pins

---
+++ frontend/src/store/pins.js
@@ -0,0 +1,60 @@
+import jwtFetch from "./jwt";
+
+
+const RECEIVE_NEW_PIN = "RECEIVE_NEW_PIN"
+const RECEIVE_EVENT_PINS = "RECEIVE_EVENT_PINS"
+const REMOVE_EVENT_PINS = "REMOVE_EVENT_PINS"
+//errors?
+
+
+
+const receiveNewPin = pin => ({
+    type: RECEIVE_NEW_PIN,
+    pin
+  });
+
+  const receiveEventPins = eventId => ({
+    type: RECEIVE_EVENT_PINS,
+    eventId
+  });
+
+  // const removeEventPins = eventId => ({
+  //   type: REMOVE_EVENT_PINS,
+  //   events
+  // });
+
+  export const fetchEventPins = eventId => async dispatch => {
+    try {
+        const res = await jwtFetch(`api/pins/${eventId}`);
+        const eventPins = await res.json();
+      dispatch(receiveEventPins(eventPins));
+    } catch (err) {
+      const resBody = await err.json();
+      // if (resBody.statusCode === 400) {
+      //   dispatch(receiveErrors(resBody.errors));
+      // }
+    }
+   
+  }
+
+  export const createPin = data => async dispatch => {
+    const res = await jwtFetch(`/api/pins/${data.event}`, {
+        method: 'POST',
+        body: JSON.stringify(data)
+      });
+      const pin = await res.json();
+      dispatch(receiveNewPin(pin));
+      //no error-handling yet
+  }
+
+  const pinsReducer = (state = {}, action) => {
+    switch(action.type) {
+        case RECEIVE_NEW_PIN:
+            return {...state, [action.pin._id]: action.pin};
+            //more cases?
+        default:
+            return state;
+    }
+  }
+
+  export default pinsReducer; //note: has not been added to rootReducer
