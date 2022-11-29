ca0500b7b5bd54b74dc5c48a54370b8068ef58a1
fee3fitri <safitri.shelton@gmail.com>
2022-11-28 18:17:15 -0800

    Event Lobby style

--- frontend/src/components/Events/EventLobby.js
+++ frontend/src/components/Events/EventLobby.js
@@ -4,8 +4,9 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { fetchEvent, loadEvent } from "../../store/events";
-import PlayGame from '../../assets/images/PlayGame.svg';
 import { Button } from "react-bootstrap";
+import Footer from '../NavBar/Footer';
+import PlayGame from '../../assets/images/PlayGame.svg';
 import defaultImage from '../../assets/images/defaultImage.svg'
 
 const EventLobby = () => {
@@ -41,56 +42,57 @@ const EventLobby = () => {
 
   return (
     <section className="event_lobby_page">
-      <div className="event_img">
-        <img src={event.image} alt={`${event.name}_image`}/>
-      </div>
-
-      <div className="event_lobby_wrapper flex-row">
-        <div className="event_details">
-          <div className="event_preview">
-            <h1>{event.name}</h1>
-            <p>By {!event.creator.username ? null : event.creator.username}</p>
-            <p><i className="fa-solid fa-dollar-sign"></i>{event.price}</p>
-          </div>
-          <div className="event_description">
-            <p>{event.description}</p>
-          </div>
-          <div className="time_location">
-            <h2>When and Where</h2>
-            <div className="flex-row">
-              <div className="time_duration">
-                <div className="time flex-row">
-                  <i className="fa-solid fa-calendar-days"></i>
-                  <div className="time_details">
-                    <p>{day}, {date}</p>
-                    <p>{hour} {ampm}</p>
+      <div className="event_lobby_wrapper">
+        <div className="event_img flex-row justify-center">
+          <img src={event.image} alt={`${event.name}_image`}/>
+        </div>
+        <div className="event_details_wrapper flex-row justify-between">
+          <div className="event_details">
+            <div className="event_preview">
+              <h1>{event.name}</h1>
+              <p>By <span>{!event.creator.username ? null : event.creator.username}</span></p>
+              <p><i className="fa-solid fa-dollar-sign"></i>{event.price} USD</p>
+            </div>
+            <div className="event_description">
+              <p>{event.description}</p>
+            </div>
+            <div className="time_location">
+              <h2>When and Where</h2>
+              <div className="flex-row justify-between">
+                <div className="time_duration">
+                  <div className="time flex-row">
+                    <i className="fa-solid fa-calendar-days"></i>
+                    <div className="time_details">
+                      <p>{day}, {date}</p>
+                      <p>{hour} {ampm}</p>
+                    </div>
+                  </div>
+                  <div className="duration flex-row">
+                    <i className="fa-solid fa-clock-rotate-left"></i>
+                    <p>{duration} hours</p>
                   </div>
                 </div>
-                <div className="duration flex-row">
-                  <i className="fa-solid fa-clock-rotate-left"></i>
-                  <p>{duration} hours</p>
+                <div className="divider"></div>
+                <div className="location flex-row">
+                  <i className="fa-solid fa-location-dot"></i>
+                  <p>{event.location}</p>
                 </div>
-              </div>
-              <div className="divider"></div>
-              <div className="location flex-row">
-                <i className="fa-solid fa-location-dot"></i>
-                <p>{event.location}</p>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="view_map flex-col align-center">
-          {/* <img src={PlayGame} alt="play game" /> */}
-          <Button onClick={openOnlineGame}>Play Online Game</Button>
-          <Button>Join the event</Button>
-          {/* <Button onClick={openLiveGame}>Play Live Game</Button> */}
+          <div className="event_buttons flex-col align-center">
+            {/* <img src={PlayGame} alt="play game" /> */}
+            <Button onClick={openOnlineGame}>Play Online Game</Button>
+            <Button>Join the event</Button>
+            {/* <Button onClick={openLiveGame}>Play Live Game</Button> */}
+          </div>
         </div>
       </div>
+
+      <Footer />
     </section>
-    
   )
 };
-
 
 export default EventLobby;
